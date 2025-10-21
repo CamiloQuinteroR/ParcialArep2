ccf79239c7ed8c6e338a5843ed7df5b8e08d05d9
unknown <camilo.quintero-r@labinfo.is.escuelaing.edu.co>
2025-10-21 11:08:22 -0500

    desarrollo del proxy

---
+++ proxy/src/main/java/com/example/proxy/ProxyController.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.proxy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author camilo.quintero-r
+ */
+@RestController
+public class ProxyController {
+    
+    
+    private static final String USER_AGENT = "Mozilla/5.0";
+    @GetMapping("/factors")
+    public String factos(@RequestParam("value") String value) throws MalformedURLException, IOException{
+        String url = "http://100.24.38.215:8081/factors?value=" + value;
+        
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            return response.toString();
+        } else {
+            System.out.println("GET request not worked");
+        }
+        return "GET DONE";
+    }
+        
+    
+    @GetMapping("/primes")
+    public String primes(@RequestParam("value") String value) throws MalformedURLException, IOException{
+        String url = "http://98.88.20.154:8080/primes?value="+value;
+        
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            return response.toString();
+        } else {
+            System.out.println("GET request not worked");
+        }
+        return "GET DONE";
+    }
+        
+        
+    }
+      
+  
+    
+    
+    
